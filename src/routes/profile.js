c8e7e35e694454a52ec17df4a68aa8c7f0b63165
Diego <soterg.uhdeia@gmail.com>
2021-03-02 23:36:17 -0500

    logout

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,1 +1,14 @@
-export default () => <span>Profile</span>;
+import authService from 'myBase'
+import React from 'react'
+
+function Profile() {
+    const onLogOutClick = () => authService.signOut();
+
+    return (
+        <div>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </div>
+    )
+}
+
+export default Profile
