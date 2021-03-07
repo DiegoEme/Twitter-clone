cf33e5072d32ccd3e06b88d60e7f5ba64106e024
Diego <soterg.uhdeia@gmail.com>
2021-03-06 21:56:27 -0500

    storing tweets in database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,37 @@
-const Home =  () => <span>Home</span>;
+import React, { useState } from "react";
+import { dbService } from "myBase";
+
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now()
+    });
+    setTweet("");
+  };
+
+  const onChange = (event) => {
+    let value = event.target.value;
+    setTweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="Whats on your mind"
+          maxLength={140}
+        />
+        <input type="submit" valur="Tweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
